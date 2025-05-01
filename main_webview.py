ed8ccdd7da0d586b3f4feca14bfcf042524defc5
Sebastian <epicsauce2013@gmail.com>
2025-04-30 19:25:42 -0700

    Not going to use the method of multiprocessing antoehr main so to keep webview talking normally within pyqt6

--- main_webview.py
+++
@@ -1,8 +0,0 @@
-from application.BackEnd.Webview.webviewmanger import *
-def get_webpage() -> None:
-
-    webview_window = WebviewWindow()
-    webview_window.set_and_start_window(None, "https://example.com")
-
-    
-get_webpage()
