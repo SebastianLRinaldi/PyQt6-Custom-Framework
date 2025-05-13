2a542f948decfbc5d1e813269bb76a28b718d579
Sebastian <epicsauce2013@gmail.com>
2025-05-13 11:33:53 -0700

    Another version of web eigne and default pyqt6 does work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-pyqt6 - v. 6.7 (G)
+pyqt6 - v. 6.7 (G) || 6.9 (G)
 pyqt6-webengine - v. 6.7 (G)
 """
 from PyQt6.QtCore import *
@@ -15,26 +15,10 @@ Test this see if anything changes in terms of speed?
 """
 # os.environ['QTWEBENGINE_CHROMIUM_FLAGS'] = '--use-gl=angle --gpu --gpu-launcher --in-process-gpu --ignore-gpu-blacklist --ignore-gpu-blocklist'
 
-# os.environ["QT_OPENGL"] = "angle"
-# os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = "--enable-gpu-rasterization --enable-zero-copy --ignore-gpu-blocklist"
-
-
-# os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = "--no-sandbox"
-# # os.environ["QT_LOGGING_RULES"] = "qt.webenginecore.debug=true"
-# os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = "--disable-gpu --disable-software-rasterizer"
-# os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = "--disable-gpu --disable-software-rasterizer"
-# os.environ["QTWEBENGINE_DISABLE_GPU"] = "1"  # Explicitly disable GPU usage
-# QCoreApplication.setAttribute(Qt.ApplicationAttribute.AA_UseSoftwareOpenGL)
-# os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = "--no-sandbox"
-# os.environ["QT_LOGGING_RULES"] = "qt.webenginecore.debug=true"
-# os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = "--disable-gpu --disable-software-rasterizer --no-sandbox"
-
-
 # Add the root directory of your project to the sys.path
 # sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 from application.FrontEnd.presentations.mySecondWindow.mySecondWindowLayout import *
 from application.FrontEnd.presentations.myFirstWindow.myFirstWindowLayout import *
-
 
 
 def main():
@@ -46,34 +30,3 @@ def main():
     
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-# # driver.execute_script("""
-# #     var dialog = document.querySelector('div[role="dialog"][aria-labelledby="dialogUpsellTitle"]');
-# #     if (dialog) {
-# #         dialog.style.display = 'none';
-# #     }
-# # """)
-
-
-# # Wait for the dialog to appear
-# try:
-#     # Wait for the element to be present in the DOM
-#     dialog = WebDriverWait(driver, 20).until(
-#         EC.presence_of_element_located((By.CSS_SELECTOR, 'div[role="dialog"][aria-labelledby="dialogUpsellTitle"]'))
-#     )
-    
-#     # Once the dialog appears, hide it
-#     driver.execute_script("""
-#         var dialog = document.querySelector('div[role="dialog"][aria-labelledby="dialogUpsellTitle"]');
-#         if (dialog) {
-#             dialog.style.display = 'none';  // Hide the dialog
-#         }
-#     """)
-#     print("Dialog hidden.")
-# except Exception as e:
-#     print(f"Error: {e}")
