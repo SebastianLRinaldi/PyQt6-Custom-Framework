842e9419c89d006ebd16555629062753afc34322
Sebastian <epicsauce2013@gmail.com>
2025-05-15 13:23:22 -0700

    Removed more of the old grid layout code from the groupers

--- application/FrontEnd/C_Grouper/SpliterGroupConfiguration.py
+++ application/FrontEnd/C_Grouper/SpliterGroupConfiguration.py
@@ -3,23 +3,12 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtGui import *
 
 class MasterSpliterGroup(QSplitter):
-    def __init__(self, orientation=Qt.Orientation.Horizontal,         
-        widgetRow: int = -1,
-        widgetCol: int = -1,
-        widgetRowSpan: int = -1,
-        widgetColSpan: int = -1):
+    def __init__(self, orientation=Qt.Orientation.Horizontal):
         super().__init__(orientation)
-        
-        self.widgetRow = widgetRow
-        self.widgetCol = widgetCol
-        self.widgetRowSpan = widgetRowSpan
-        self.widgetColSpan = widgetColSpan
-        
         
         self.setOpaqueResize(True)  # Enable smooth resizing
         self.setChildrenCollapsible(True)  # Prevent widgets from being collapsed
         self.setHandleWidth(5)  # Set handle width for better visibility
-        
         
         # Style the handles
         self.setStyleSheet("""
@@ -30,7 +19,6 @@ class MasterSpliterGroup(QSplitter):
             }
         """)
         
-            
     def add_widgets_to_spliter(self, *widgets):
         if not all(isinstance(widget, QWidget) for widget in widgets):
             raise TypeError("All items must be instances of QWidget.")
--- application/FrontEnd/C_Grouper/TabGroupConfigureation.py
+++ application/FrontEnd/C_Grouper/TabGroupConfigureation.py
@@ -5,25 +5,13 @@ from PyQt6.QtGui import *
 from application.FrontEnd.C_Grouper.widgetGroupFrameworks import WidgetGroup
 
 class TabHolder(QTabWidget):
-    def __init__(
-        self,
-        widgetRow: int = -1,
-        widgetCol: int = -1,
-        widgetRowSpan: int = -1,
-        widgetColSpan: int = -1,
-        title: str = "",
-    ):
+    def __init__(self, title: str = "",):
         super().__init__()
-        self._parent = None
-        self.widgetRow = widgetRow
-        self.widgetCol = widgetCol
-        self.widgetRowSpan = widgetRowSpan
-        self.widgetColSpan = widgetColSpan
+        
         self.title = title
         self.setTabShape(QTabWidget.TabShape.Triangular)
         self.setMovable(True)
         
-    
     def add_groups_as_tabs(self, *pages: WidgetGroup):
         """Add tabs to the master tab widget for the group of widgets as pages"""
         for page in pages:
