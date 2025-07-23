799fe200c0a038a44bdf9e6baaad1ad886efc1e6
Sebastian <epicsauce2013@gmail.com>
2025-07-23 16:53:46 -0700

    Realized that I would need a common command for making it easier to navigate logic soI added that as a note

--- src/apps/app0/Functions.py
+++ src/apps/app0/Functions.py
@@ -4,6 +4,14 @@ from PyQt6.QtGui import *
 
 from .Layout import Layout
 
+"""
+Close methods
+Ctrl+k + Ctrl+0
+
+Open Methods 
+Ctrl+k + Ctrl+J
+"""
+
 class Logic:
     def __init__(self, ui: Layout):
         self.ui = ui
