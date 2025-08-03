edbfe36fdcfb395f3ec1dd0856e1f32e61f18f17
Sebastian <epicsauce2013@gmail.com>
2025-08-03 15:04:22 -0700

    Quick naming case fix

--- src/components/blank/logic.py
+++ src/components/blank/logic.py
@@ -6,6 +6,14 @@ from .layout import Layout
 from src.components import *
 from src.helpers import *
 
+"""
+Close methods
+Ctrl+k + Ctrl+0
+
+Open Methods 
+Ctrl+k + Ctrl+J
+"""
+
 class Logic:
     def __init__(self, ui: Layout):
         self.ui = ui
